47ce953fbc92a0072c5fa672252b7301a4a86479
vidsag <vidhyasagar.mohanraj@gmail.com>
2021-09-28 14:11:53 +0530

    [Sagar] app:

--- android/app/src/main/java/com/instabugtest/BaseReactActivity.java
+++ android/app/src/main/java/com/instabugtest/BaseReactActivity.java
@@ -6,13 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactRootView;
 import com.facebook.react.common.LifecycleState;
 import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
 import com.facebook.react.shell.MainReactPackage;
+import com.instabug.reactlibrary.RNInstabugReactnativePackage;
 
-public class BaseReactActivity extends Activity implements DefaultHardwareBackBtnHandler {
+public class BaseReactActivity extends AppCompatActivity implements DefaultHardwareBackBtnHandler {
 
     private ReactRootView mReactRootView;
     protected static ReactInstanceManager sReactInstanceManager;
@@ -20,18 +24,7 @@ public class BaseReactActivity extends Activity implements DefaultHardwareBackBt
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        if (sReactInstanceManager == null) {
-            sReactInstanceManager = ReactInstanceManager.builder()
-                    .setApplication(getApplication())
-                    .setCurrentActivity(this)
-                    .setBundleAssetName("index.android.bundle")
-                    .setJSMainModulePath("index")
-                    .addPackage(new MainReactPackage())
-                    .setUseDeveloperSupport(BuildConfig.DEBUG)
-                    .setInitialLifecycleState(LifecycleState.RESUMED)
-                    .build();
-        }
+        sReactInstanceManager = TestApplication.sReactInstanceManager;
     }
 
     protected void startReactView(String componentName) {
--- android/app/src/main/java/com/instabugtest/TestApplication.java
+++ android/app/src/main/java/com/instabugtest/TestApplication.java
@@ -2,18 +2,33 @@ package com.instabugtest;
 
 import android.app.Application;
 
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.common.LifecycleState;
+import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.instabug.reactlibrary.RNInstabugReactnativePackage;
 
 public class TestApplication  extends Application {
 
+
+    public static ReactInstanceManager sReactInstanceManager;
+
     @Override
     public void onCreate() {
         new RNInstabugReactnativePackage
-                .Builder("TOKEN", TestApplication.this)
+                .Builder("8a99cf713bfabbbb6a7814acb10a84e1", TestApplication.this)
                 .setInvocationEvent("none")
                 .build();
         super.onCreate();
         SoLoader.init(this,false);
+        sReactInstanceManager = ReactInstanceManager.builder()
+                .setApplication(this)
+                .setBundleAssetName("index.android.bundle")
+                .setJSMainModulePath("index")
+                .addPackage(new RNInstabugReactnativePackage())
+                .addPackage(new MainReactPackage())
+                .setUseDeveloperSupport(BuildConfig.DEBUG)
+                .setInitialLifecycleState(LifecycleState.BEFORE_RESUME)
+                .build();
     }
 }
